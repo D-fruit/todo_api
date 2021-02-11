ff63ec7073bd9c1d6694bff885982958c6761ac4
ryunakae <ryunakae@gmail.com>
2021-02-12 00:08:50 +0900

    initial commit

---
+++ app/Http/Controllers/ToDoItemsController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ToDoItem;
+use Illuminate\Http\Request;
+
+class ToDoItemsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = ToDoItem::all();
+        return response()->json([
+            'message' => 'OK',
+            'data' => $items
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $item = new ToDoItem;
+        $item->item = $request->item;
+        $item->save();
+        return response()->json([
+            'message' => 'Created successfully',
+            'data' => $item
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ToDoItem  $to_do_item
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ToDoItem  $toDoItem
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $item = ToDoItem::find($id);
+        $item->item = $request->item;
+        $item->save();
+        if ($item) {
+            return response()->json([
+                'message' => 'Updated successfully',
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Not found',
+            ], 404);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ToDoItem  $toDoItem
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $item = ToDoItem::find($id)->delete();
+        if ($item) {
+            return response()->json([
+                'message' => 'Deleted successfully',
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Not found',
+            ], 404);
+        }
+    }
+}
---
+++ database/migrations/2021_02_08_132603_create_to_do_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateToDoItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('to_do_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('item');
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+            $table->timestamp('created_at')->useCurrent()->nullable();
+
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('to_do_items');
+    }
+}
